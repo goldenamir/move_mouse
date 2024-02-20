e1f219a6fa3915cd694a1eac1ecb419a48bb681b
Amir Taghizadeh Vahed <emir.taghizadeh@gmail.com>
2024-02-20 12:52:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random, pyautogui, time
 
 while True:
-    x = random.randint(1, 1928)
-    y = random.randint(1, 1888)
+    x = random.randint(1, 1000)
+    y = random.randint(1, 1200)
     pyautogui.moveTo(x,y, 2)
     time.sleep(200)
